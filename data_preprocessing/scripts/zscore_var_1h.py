e4796e88d2639edb6249899dcefafaa5052d90f3
Yingkai Sha <yingkaisha@gmail.com>
2024-08-09 10:37:29 -0600

    add notebook and script versions

---
+++ data_preprocessing/scripts/zscore_var_1h.py
@@ -0,0 +1,24 @@
+import os
+import sys
+import yaml
+import numpy as np
+import xarray as xr
+
+sys.path.insert(0, os.path.realpath('../../libs/'))
+import preprocess_utils as pu
+
+# parse input
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('varname', help='varname')
+args = vars(parser.parse_args())
+
+varname = args['varname']
+
+config_name = os.path.realpath('../data_config_1h.yml')
+
+with open(config_name, 'r') as stream:
+    conf = yaml.safe_load(stream)
+
+pu.zscore_by_var(conf, varname)
+
