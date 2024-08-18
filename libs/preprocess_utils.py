df95921d7ee99b7a973bfca28f47e7253f229a65
Yingkai Sha <yingkaisha@gmail.com>
2024-08-17 18:29:14 -0600

    add residual norm function

--- libs/preprocess_utils.py
+++ libs/preprocess_utils.py
@@ -15,12 +15,84 @@ def get_forward_data(filename) -> xr.DataArray:
         dataset = xr.open_zarr(filename, consolidated=True)
     return dataset
 
-def zscore_by_var(conf, varname):
+# def zscore_surface_var(conf, varname):
+#     '''
+#     Compute mean and variance (can be converted to std) from yearly zarr or nc files
+#     It combines two yearly files iteratively using the pooling equations:
+#     https://math.stackexchange.com/questions/2971315/how-do-i-combine-standard-deviations-of-two-groups
+#     The function relies on one of the credit-data/data_preprocessing/data_config.yml
+
+#     This function works for surface variables (no level coordinate)
+#     Input coordinates should be (time, longitude, latitude)
+#     XXXXXXXXXXXX combined to zscore_surface_var xxxxxxxxxxxxxxx
+#     '''
+#     # ------------------------------------------------------------------------------------ #
+#     # lists yearly files and open as xr.Dataset
+#     filenames = sorted(glob(conf['zscore'][varname]))
+    
+#     year_range = conf['zscore']['years_range']
+#     train_years = [str(year) for year in range(year_range[0], year_range[1])]
+#     train_files = [file for file in filenames if any(year in file for year in train_years)]
+    
+#     list_ds_train = []
+    
+#     for fn in train_files:
+#         list_ds_train.append(get_forward_data(fn))
+        
+#     # ------------------------------------------------------------------------------------ #
+#     ds_example = list_ds_train[0][varname]
+#     var_shape = ds_example.shape
+    
+#     N_grids = var_shape[-1] * var_shape[-2]
+#     mean_std_save = np.empty((2,))
+#     mean_std_save.fill(np.nan)
+    
+#     # ------------------------------------------------------------------------------------ #
+#     # loop thorugh files and compute mean and std 
+#     for i_fn, ds in enumerate(list_ds_train):
+        
+#         # get the xr.Dataset per var per level
+#         ds_subset = ds[varname]
+        
+#         # get mean and var for the current year
+#         mean_current_yr = float(ds_subset.mean())
+#         var_current_yr = float(ds_subset.var())
+#         L = len(ds_subset) * N_grids
+        
+#         print('{} - {}'.format(mean_current_yr, var_current_yr))
+            
+#         if i_fn == 0:
+#             # if it is the first year, pass current year to the combined 
+#             mean_std_save[0] = mean_current_yr
+#             mean_std_save[1] = var_current_yr
+#             N_samples = L
+            
+#         else:
+#             # https://math.stackexchange.com/questions/2971315/how-do-i-combine-standard-deviations-of-two-groups
+#             mean_new = (L * mean_current_yr + N_samples * mean_std_save[0]) / (L + N_samples)
+#             var_new = ((L - 1) * var_current_yr + (N_samples - 1) * mean_std_save[1]) / (L + N_samples - 1)
+#             var_new_adjust = (L * N_samples * (mean_current_yr - mean_std_save[0])**2) / (L + N_samples) / (L + N_samples -1)
+            
+#             mean_std_save[0] = mean_new
+#             mean_std_save[1] = var_new + var_new_adjust
+#             N_samples = N_samples + L
+            
+#             print('{} - {}'.format(mean_std_save[0], mean_std_save[1]))
+    
+#     save_name = conf['zscore']['save_loc'] + '{}_mean_std_{}.npy'.format(conf['zscore']['prefix'], varname)
+#     print('Save to {}'.format(save_name))
+#     np.save(save_name, mean_std_save)
+
+
+def zscore_var(conf, varname, ind_level=None):
     '''
     Compute mean and variance (can be converted to std) from yearly zarr or nc files
     It combines two yearly files iteratively using the pooling equations:
     https://math.stackexchange.com/questions/2971315/how-do-i-combine-standard-deviations-of-two-groups
     The function relies on one of the credit-data/data_preprocessing/data_config.yml
+
+    This function works for all variables
+    Input coordinates should be (time, level, longitude, latitude)
     '''
     # ------------------------------------------------------------------------------------ #
     # lists yearly files and open as xr.Dataset
@@ -38,102 +110,144 @@ def zscore_by_var(conf, varname):
     # ------------------------------------------------------------------------------------ #
     # determine if the var has levels
     ds_example = list_ds_train[0][varname]
-    
     var_shape = ds_example.shape
-    
-    if len(var_shape) == 4:
-        flag_has_level = True
-    elif len(var_shape) == 3:
-        flag_has_level = False
-    else:
-        print('data shape {} is not accepted, need to have at least (time, lat, lon)'.format(var_shape))
-        raise
-    
     N_grids = var_shape[-1] * var_shape[-2]
-    
-    if flag_has_level:
-        N_levels = var_shape[1]
-        mean_std_save = np.empty((2, N_levels))
-        mean_std_save.fill(np.nan)
-        N_samples = np.empty((N_levels,))
-        N_samples.fill(np.nan)
-    
-    else:
-        mean_std_save = np.empty((2,))
-        mean_std_save.fill(np.nan)
+    mean_std_save = np.empty((2,))
+    mean_std_save.fill(np.nan)
 
     # ------------------------------------------------------------------------------------ #
-    # if has level:
-    if flag_has_level:
+    # loop thorugh files and compute mean and std 
+    for i_fn, ds in enumerate(list_ds_train):
         
-        # loop thorugh files and compute mean and std 
-        for i_fn, ds in enumerate(list_ds_train):
-            
-            # loop through levels
-            for i_level in range(N_levels):
-                
-                # get the xr.Dataset per var per level
-                ds_subset = ds[varname].isel(level=i_level)
-                
-                # get mean and var for the current year
-                mean_current_yr = float(ds_subset.mean())
-                var_current_yr = float(ds_subset.var())
-                L = len(ds_subset) * N_grids
-                
-                print('level {} current {} - {}'.format(i_level, mean_current_yr, var_current_yr))
-                    
-                if i_fn == 0:
-                    # if it is the first year, pass current year to the combined 
-                    mean_std_save[0, i_level] = mean_current_yr
-                    mean_std_save[1, i_level] = var_current_yr
-                    N_samples[i_level] = L
-                    
-                else:
-                    # https://math.stackexchange.com/questions/2971315/how-do-i-combine-standard-deviations-of-two-groups
-                    mean_new = (L * mean_current_yr + N_samples[i_level] * mean_std_save[0, i_level]) / (L + N_samples[i_level])
-                    var_new = ((L - 1) * var_current_yr + (N_samples[i_level] - 1) * mean_std_save[1, i_level]) / (L + N_samples[i_level] - 1)
-                    var_new_adjust = (L * N_samples[i_level] * (mean_current_yr - mean_std_save[0, i_level])**2) / (L + N_samples[i_level]) / (L + N_samples[i_level] - 1)
-                    
-                    mean_std_save[0, i_level] = mean_new
-                    mean_std_save[1, i_level] = var_new + var_new_adjust
-                    N_samples[i_level] = N_samples[i_level] + L
-                    
-                    print('level {} combine {} - {}'.format(i_level, mean_std_save[0, i_level], mean_std_save[1, i_level]))
-                        
-    # ------------------------------------------------------------------------------------ #
-    # if no level
-    else:
-        # loop thorugh files and compute mean and std 
-        for i_fn, ds in enumerate(list_ds_train):
-            
-            # get the xr.Dataset per var per level
+        # get the xr.Dataset per var per level
+        if ind_level is not None:
+            ds_subset = ds[varname].isel(level=ind_level)
+        else:
             ds_subset = ds[varname]
+        
+        # get mean and var for the current year
+        mean_current_yr = float(ds_subset.mean())
+        var_current_yr = float(ds_subset.var())
+        L = len(ds_subset) * N_grids
+        
+        print('{} - {}'.format(mean_current_yr, var_current_yr))
             
-            # get mean and var for the current year
-            mean_current_yr = float(ds_subset.mean())
-            var_current_yr = float(ds_subset.var())
-            L = len(ds_subset) * N_grids
+        if i_fn == 0:
+            # if it is the first year, pass current year to the combined 
+            mean_std_save[0] = mean_current_yr
+            mean_std_save[1] = var_current_yr
+            N_samples = L
             
-            print('{} - {}'.format(mean_current_yr, var_current_yr))
-                
-            if i_fn == 0:
-                # if it is the first year, pass current year to the combined 
-                mean_std_save[0] = mean_current_yr
-                mean_std_save[1] = var_current_yr
-                N_samples = L
-                
-            else:
-                # https://math.stackexchange.com/questions/2971315/how-do-i-combine-standard-deviations-of-two-groups
-                mean_new = (L * mean_current_yr + N_samples * mean_std_save[0]) / (L + N_samples)
-                var_new = ((L - 1) * var_current_yr + (N_samples - 1) * mean_std_save[1]) / (L + N_samples - 1)
-                var_new_adjust = (L * N_samples * (mean_current_yr - mean_std_save[0])**2) / (L + N_samples) / (L + N_samples -1)
-                
-                mean_std_save[0] = mean_new
-                mean_std_save[1] = var_new + var_new_adjust
-                N_samples = N_samples + L
-                
-                print('{} - {}'.format(mean_std_save[0], mean_std_save[1]))
-
-    save_name = conf['zscore']['save_loc'] + '{}_mean_std_{}.npy'.format(conf['zscore']['prefix'], varname)
+        else:
+            # https://math.stackexchange.com/questions/2971315/how-do-i-combine-standard-deviations-of-two-groups
+            mean_new = (L * mean_current_yr + N_samples * mean_std_save[0]) / (L + N_samples)
+            var_new = ((L - 1) * var_current_yr + (N_samples - 1) * mean_std_save[1]) / (L + N_samples - 1)
+            var_new_adjust = (L * N_samples * (mean_current_yr - mean_std_save[0])**2) / (L + N_samples) / (L + N_samples -1)
+            
+            mean_std_save[0] = mean_new
+            mean_std_save[1] = var_new + var_new_adjust
+            N_samples = N_samples + L
+            
+            print('{} - {}'.format(mean_std_save[0], mean_std_save[1]))
+            
+    if ind_level is not None:
+        save_name = conf['zscore']['save_loc'] + '{}_level{}_mean_std_{}.npy'.format(conf['zscore']['prefix'], ind_level+1, varname)
+    else:
+        save_name = conf['zscore']['save_loc'] + '{}_mean_std_{}.npy'.format(conf['zscore']['prefix'], varname)
+        
     print('Save to {}'.format(save_name))
     np.save(save_name, mean_std_save)
+
+def residual_zscore_var(conf, varname, ind_level=None):
+
+    filenames = sorted(glob(conf['residual'][varname]))
+    
+    year_range = conf['residual']['years_range']
+    train_years = [str(year) for year in range(year_range[0], year_range[1])]
+    train_files = [file for file in filenames if any(year in file for year in train_years)]
+    
+    list_ds_train = []
+    
+    for fn in train_files:
+        list_ds_train.append(get_forward_data(fn))
+        
+    # ------------------------------------------------------------------------------------ #
+    ds_example = list_ds_train[0][varname]
+    var_shape = ds_example.shape
+    
+    N_grids = var_shape[-1] * var_shape[-2]
+    mean_std_save = np.empty((2,))
+    mean_std_save.fill(np.nan)
+
+    # ------------------------------------------------------------------------------------ #
+    # mean, std
+    ds_mean = xr.open_dataset(conf['residual']['mean_loc'])
+    ds_std = xr.open_dataset(conf['residual']['std_loc'])
+    
+    if ind_level is not None:
+        ds_mean = ds_mean.isel(level=ind_level)
+        ds_std = ds_std.isel(level=ind_level)
+
+    for i_fn, ds in enumerate(list_ds_train):
+        
+        if ind_level is not None:
+            ds = ds.isel(level=ind_level)
+            
+        # ===================================================================== #
+        # apply np.diff
+        var_diff = xr.apply_ufunc(
+            np.diff,
+            (ds[varname] - ds_mean[varname]) / ds_std[varname],
+            input_core_dims=[['time']],
+            output_core_dims=[['time_diff']],  # Change this to a new dimension name
+            vectorize=True,
+            dask='allowed',
+            output_dtypes=[ds[varname].dtype]
+        )
+        
+        ds_out = var_diff.to_dataset(name='{}_diff'.format(varname))
+        
+        ds_out = ds_out.assign_coords(
+            time_diff=ds_out['time_diff'])
+        
+        ds_out = ds_out.transpose("time_diff", "latitude", "longitude")
+        
+        # ===================================================================== #
+        # compute the mean and std from the np.diff result
+        
+        ds_subset = ds_out['{}_diff'.format(varname)]
+        
+        # get mean and var for the current year
+        mean_current_yr = float(ds_subset.mean())
+        var_current_yr = float(ds_subset.var())
+        L = len(ds_subset) * N_grids
+        
+        print('{} - {}'.format(mean_current_yr, var_current_yr))
+            
+        if i_fn == 0:
+            # if it is the first year, pass current year to the combined 
+            mean_std_save[0] = mean_current_yr
+            mean_std_save[1] = var_current_yr
+            N_samples = L
+            
+        else:
+            # https://math.stackexchange.com/questions/2971315/how-do-i-combine-standard-deviations-of-two-groups
+            mean_new = (L * mean_current_yr + N_samples * mean_std_save[0]) / (L + N_samples)
+            var_new = ((L - 1) * var_current_yr + (N_samples - 1) * mean_std_save[1]) / (L + N_samples - 1)
+            var_new_adjust = (L * N_samples * (mean_current_yr - mean_std_save[0])**2) / (L + N_samples) / (L + N_samples -1)
+            
+            mean_std_save[0] = mean_new
+            mean_std_save[1] = var_new + var_new_adjust
+            N_samples = N_samples + L
+            
+            print('{} - {}'.format(mean_std_save[0], mean_std_save[1]))
+    
+    if ind_level is not None:
+        save_name = conf['residual']['save_loc'] + '{}_level{}_mean_std_{}.npy'.format(conf['residual']['prefix'], ind_level+1, varname)
+    else:
+        save_name = conf['residual']['save_loc'] + '{}_mean_std_{}.npy'.format(conf['residual']['prefix'], varname)
+        
+    print('Save to {}'.format(save_name))
+    np.save(save_name, mean_std_save)
+
+
